c0a738ea4e9cfd7af02967a024825df4d9a18193
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-05 17:46:21 -0300

    Logic #2

--- App.js
+++ App.js
@@ -39,7 +39,28 @@ export default class App extends Component {
   }
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    }
+    else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } 
+      catch (e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      })
+    }
   }
 
   render() {
