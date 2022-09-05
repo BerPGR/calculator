251d7934133379cd96e8a8c054ed0f04113175e5
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-05 17:36:39 -0300

    Logic #1

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
-
+import { StyleSheet,View } from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
 export default class App extends Component {
 
-  state = {
-    displayValue: '0'
-  }
+  state = {...initialState}
 
   addDigit = n => {
-    this.setState({displayValue: n})
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue;
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue: '0'})
+    this.setState({...initialState})
   }
 
   setOperation = operation => {
